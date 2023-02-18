f39204a6840b0304c08e164a6135b909f1f673c1
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-02-18 22:03:51 +0400

    Fetch problem remains

--- src/React-Router@6.4/Pages/сareers/Careers.jsx
+++ src/React-Router@6.4/Pages/сareers/Careers.jsx
@@ -3,11 +3,8 @@ import { Link, useLoaderData } from 'react-router-dom';
 import useTitleSetter from '../../utils/titleSetter';
 
 export default function Careers() {
-  const { careers } = useLoaderData();
+  const careers = useLoaderData();
   // this is name is as you wish
-  // console.log();
-
-  // console.log(careers);
 
   useTitleSetter('Careers');
 
@@ -29,10 +26,11 @@ export default function Careers() {
 // Loader function
 export const careersLoader = async () => {
   try {
-    // const { data } = await axios.get('http://localhost:4000/careers');
-    const { data } = await axios.get('data/data.json');
-    console.log(data);
-    return data;
+    // const { data } = await axios.get('http://localhost:5000/careers');
+    const {
+      data: { careers },
+    } = await axios.get('data/data.json');
+    return careers;
   } catch (err) {
     throw Error("Couldn't fetch careers data");
   }
