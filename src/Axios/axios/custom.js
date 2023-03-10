e3877632698f5f6832494649845402d73ecbe4ea
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-03-10 23:06:56 +0400

    switch-dark-mode

--- src/Axios/axios/custom.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const authFetch = axios.create({
-  baseURL: 'https://course-api.com/',
-  headers: {
-    Accept: 'application/json',
-  },
-});
--- src/Axios/reqs/exporter.js
+++
@@ -1,4 +0,0 @@
-import FirstRequest from './FirstReq';
-import Headers from './Header';
-
-export { FirstRequest, Headers };
