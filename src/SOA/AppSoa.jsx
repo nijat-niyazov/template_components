0efaa7db49bc9459988901dd38cc92bc8787ee0e
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-03-18 15:02:44 +0400

    Animation On Scroll understood

---
+++ src/SOA/AppSoa.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './com/Card';
+// import './com/style.scss'
+
+const AppSoa = () => {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+};
+
+export default AppSoa;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppIntObs from './Functions&Hooks/intersectionObserver/AppIntObs';
+import AppRouter from './React-Router@6.4/AppRouter';
 import './index.css';
+import AppSoa from './SOA/AppSoa';
 // import AppImperative from './Functions&Hooks/ImperativeHandle/AppImperative';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <AppIntObs />
+    <AppSoa />
   </>
 );
