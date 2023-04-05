442ab4ff142a75a71fbfd49e83fdc2e1dcb00bf6
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-04-05 11:26:02 +0400

    formik handling, manual validations, fields,fieldarrays, error handlers

---
+++ formik/src/App.jsx
@@ -0,0 +1,12 @@
+import ModernForm from './simpleForms/Form';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ModernForm />
+    </div>
+  );
+}
+
+export default App;
