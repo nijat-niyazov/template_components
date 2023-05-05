017a5449ef203d42153630d46d0a95cf5e8ab6d3
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-05-05 19:32:25 +0400

    table-tailwind-implementation

--- table-data-react/src/components/Table.jsx
+++ table-data-react/src/components/Table.jsx
@@ -67,9 +67,9 @@ const Table = () => {
     setData(DATA.slice(showTables * (activePage - 1), result));
   }, [result]);
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
   // ========== CHANGE SHOW TABLES ===============
 
@@ -123,30 +123,103 @@ const Table = () => {
 
   return (
     <>
-      <div className="tasks">
+      <div className="flex m-auto w-[95%] mt-[10px]">
         {tasks?.map((task, i) => {
           return (
             <div
-              className={`task  ${task.checked ? 'checked' : ''}`}
               key={task.id}
+              className="mr-[20px]  width-[150px] text-center flex place-items-center justify-center  "
             >
-              <span>{task.task}</span>
-              <button onClick={() => checked(task.id)}>✅</button>
+              <span
+                className={`ease-out duration-300   bg-red-400 text-black p-4 rounded-xl ${
+                  task.checked && 'opacity-50 brightness-50 line-through'
+                }`}
+              >
+                {task.task}
+              </span>
+              <button
+                className="ml-[5px] bg-none border-none"
+                onClick={() => checked(task.id)}
+              >
+                ✅
+              </button>
             </div>
           );
         })}
       </div>
-      <div className="results">
-        <span>
+      <div className="w-[30%] my-[20px] mx-auto bg-pink-300 flex items-center justify-center rounded-2xl ">
+        <span className="py-[10px] px-[20px] text-[30px]">
           Show results {data.length !== 0 && data[0].id} -{' '}
           {result > DATA.length ? DATA.length : result} of {DATA.length}
         </span>
         <br />
       </div>
 
-      <table>
+      <table className="border-collapse w-[98%] m-auto">
         <thead>
-          <tr className="header-row">
+          <tr className="bg-blue-300">
+            {headers.map((header, i) => (
+              <td
+                className="border-black border-solid border-2 p-[10px]"
+                key={i}
+              >
+                <li className="text-white w-full px-[10px] border-none bg-transparent list-none flex items-center justify-center">
+                  {header.toUpperCase()}
+                  {header === 'first_name' && (
+                    <input
+                      type="text"
+                      autoFocus
+                      value={search}
+                      ref={searchRef}
+                      onChange={e => setSearch(e.target.value)}
+                    />
+                  )}
+                  {canBeSorted.includes(header) && (
+                    <button
+                      className="ml-[10px] cursor-pointer"
+                      onClick={() => sortByToggle(header)}
+                    >
+                      sort
+                    </button>
+                  )}
+                  {sorted !== 0 && (
+                    <span className="ml-[5px]">
+                      {' '}
+                      {ascending ? '🔼' : '🔽'}{' '}
+                    </span>
+                  )}
+                </li>
+              </td>
+            ))}
+          </tr>
+        </thead>
+        {/* <thead>
+         
+        </thead> */}
+
+        <tbody>
+          {data.map((cell, i) => {
+            const datas = Object.values(cell);
+            return (
+              <tr
+                className={`p-[10px] ${(i + 1) % 2 === 0 && 'bg-pink-300'}`}
+                key={cell.id}
+              >
+                {datas.map((data, i) => (
+                  <td
+                    className="border-solid border-black border-2 font-bold"
+                    key={i}
+                  >
+                    {data}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+
+        <tfoot className="bg-blue-300">
+          <tr>
             {headers.map((header, i) => (
               <td key={i}>
                 <li>
@@ -168,50 +241,29 @@ const Table = () => {
               </td>
             ))}
           </tr>
-        </thead>
-
-        <tbody>
-          {data.map(cell => {
-            const datas = Object.values(cell);
-            return (
-              <tr key={cell.id}>
-                {datas.map((data, i) => (
-                  <td key={i}>{data}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            {headers.map((header, i) => (
-              <td key={i}>
-                <li>{header.toUpperCase()}</li>
-              </td>
-            ))}
-          </tr>
         </tfoot>
       </table>
+
       {activePage === 1 && (
-        <div className="showTables">
-          <label htmlFor="">Show {showTables} </label> <br />
+        <div className="w-[20%] m-auto flex flex-col items-center justify-center">
+          <label>Show {showTables} </label> <br />
           <input
             type="range"
             value={showTables}
-            max={DATA.length}
+            max={DATA?.length}
             min={5}
             onChange={changeShowTables}
           />
         </div>
       )}
-      <section className="pagination">
+      <section className="w-[50%] my-[20px] mx-auto flex items-center justify-around bg-pink-300 p-[20px] rounded-[20px] ">
         {activePage !== 1 && (
           <button onClick={() => setActivePage(p => p - 1)}>Previous ⏮</button>
         )}
         <div className="numbs" ref={activeRef}>
           {Array.from({ length: pageNums }, (_, i) => (
             <button
-              className="pageNum"
+              className="border-none text-[20px] bg-brown-400 rounded-full py-[5px] px-[10px] cursor-pointer text-white ease-in-out duration-300 mr-[10px] opacity-100 hover:opacity-50   "
               onClick={e => setActivePage(Number(e.target.innerText))}
               key={i}
             >
