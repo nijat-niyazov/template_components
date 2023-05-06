eec9b8bc627b5f3b279d3fdcd6a9c3ff477eb400
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-05-06 15:14:46 +0400

    tailwind style waits

--- table-data-react/src/App.jsx
+++ table-data-react/src/App.jsx
@@ -1,18 +1,8 @@
-import Example1 from './components/Example1';
-import Example2 from './components/Example2';
-import Example3 from './components/Example3';
-import Table from './components/Table';
-import data from './data/MOCK_DATA (9).json'
+import Table from "./components/Table";
 
 function App() {
   return (
-    <div>
-      <Table/>
-      {/* <Example1 />
-      
-      <Example2 /> */}
-      {/* <Example3 data={data}/> */}
-    </div>
+      <Table />
   );
 }
 
